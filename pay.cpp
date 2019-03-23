13f130e8383d4b6a26a7103afeb8ceaa26d3bfc8
sahilsheth <32227270+sahilsheth@users.noreply.github.com>
2019-03-23 11:43:48 -0700

    Update pay.cpp

--- pay.cpp
+++ pay.cpp
@@ -1,56 +1,68 @@
+//Sahil Sheth
+//CPSC 301-01
+
 #include "person.cpp"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
 
 
-void readData(string fileName, Person a[], int size);
-void writeData(string filename, Person a[], int lines);
+void readData(vector<Person> &a);
+void writeData(vector<Person> &a);
 
 
 int main()
 {
-  int N = 6;
-  Person individual[20];
-  readData("input.txt", individual, N);
-  writeData("output.txt", individual, N);
+  vector<Person> individual;
+  readData(individual);
+  writeData(individual);
   return 0;
 }
 
-void readData(string fileName, Person a[], int size)
+void readData(vector<Person> &a)
 {
-  ifstream myfile;
-
-    string first, last;
+    string first, last, line;
     float pay, hours;
-    myfile.open(fileName.c_str());
-    for(int i = 0; i < size; i++)
+    ifstream myfile;
+    myfile.open("input.txt");
+     while(!myfile.eof())
     {
-
         myfile >> first >> last >> pay >> hours;
-        a[i].setFirstName(first);
-        a[i].setLastName(last);
-        a[i].setPayRate(pay);
-        a[i].setHoursWorked(hours);
-
-        cout << first << " " << last << " " << " " << pay << " " << hours <<endl;
-
-      }
+        getline(myfile, line);
+        a.emplace_back(first, last, pay, hours);
+    }
+  
+   /* for(unsigned int i = 0; i<a.size(); i++)
+  {
+      cout << a.at(i).getFirstName() << endl;
+      cout << a.at(i).getLastName() << endl;
+      cout << a.at(i).getPayRate() << endl;
+      cout << a.at(i).getHoursWorked() << endl;
+      //cout << a.at(i).getPayRate() << endl;
+  }*/
+  
+  myfile.close();
 
 
 }
 
-void writeData(string filename, Person a[], int lines)
+void writeData(vector<Person> &a)
 {
   ofstream newFile;
-  newFile.open(filename.c_str());
-  if (!newFile.eof())
-  {
-    for(int i = 0; i < lines; i++)
+  newFile.open("output.txt");
+  string writeName;
+  float writeAmount;
+    for(int i = 0; i < a.size(); i++)
     {
-        newFile << a[i].fullName() << "  " << a[i].totalPay() << endl;
+        writeName = a[i].fullName();
+        writeAmount = a[i].totalPay();
+        newFile << writeName << " " << writeAmount << endl;
+        cout << writeName << " " << writeAmount << endl;
     }
-  }
+  
+    newFile.close();
 }
